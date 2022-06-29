5cc81200589e023d71188474bd9635b9699c257e
yuxuan li <zcablii@ucl.ac.uk>
2022-06-29 10:23:35 +0800

    jittor diffaug

--- SPADE_jittor/util/util.py
+++ SPADE_jittor/util/util.py
@@ -21,29 +21,29 @@ def DiffAugment(real_img, fake_img, label, policy=''):
         for p in policy.split(','):
             for f in AUGMENT_FNS[p]:
                 real_img, fake_img, label = f(real_img, fake_img, label)
-        real_img, fake_img = real_img.contiguous(), fake_img.contiguous()
+        real_img, fake_img = real_img, fake_img
     return real_img, fake_img, label
 
 
 def rand_brightness(real_img, fake_img, label, strength=1.):
-    real_img = real_img + (jt.rand(real_img.size(0), 1, 1, 1, dtype=real_img.dtype, device=real_img.device) - 0.5)*strength
-    fake_img = fake_img + (jt.rand(fake_img.size(0), 1, 1, 1, dtype=fake_img.dtype, device=fake_img.device) - 0.5)*strength
+    real_img = real_img + (jt.rand(real_img.size(0), 1, 1, 1) - 0.5)*strength
+    fake_img = fake_img + (jt.rand(fake_img.size(0), 1, 1, 1) - 0.5)*strength
     return real_img, fake_img, label
 
 
 def rand_saturation(real_img, fake_img, label):
-    real_img_mean = real_img.mean(dim=1, keepdim=True)
-    real_img = (real_img - real_img_mean) * (jt.rand(real_img.size(0), 1, 1, 1, dtype=real_img.dtype, device=real_img.device) * 2) + real_img_mean
-    fake_img_mean = fake_img.mean(dim=1, keepdim=True)
-    fake_img = (fake_img - fake_img_mean) * (jt.rand(fake_img.size(0), 1, 1, 1, dtype=fake_img.dtype, device=fake_img.device) * 2) + fake_img_mean
+    real_img_mean = real_img.mean(dim=1, keepdims=True)
+    real_img = (real_img - real_img_mean) * (jt.rand(real_img.size(0), 1, 1, 1) * 2) + real_img_mean
+    fake_img_mean = fake_img.mean(dim=1, keepdims=True)
+    fake_img = (fake_img - fake_img_mean) * (jt.rand(fake_img.size(0), 1, 1, 1) * 2) + fake_img_mean
     return real_img, fake_img, label
 
 
 def rand_contrast(real_img, fake_img, label):
-    real_img_mean = real_img.mean(dim=[1, 2, 3], keepdim=True)
-    real_img = (real_img - real_img_mean) * (jt.rand(real_img.size(0), 1, 1, 1, dtype=real_img.dtype, device=real_img.device) + 0.5) + real_img_mean
-    fake_img_mean = fake_img.mean(dim=[1, 2, 3], keepdim=True)
-    fake_img = (fake_img - fake_img_mean) * (jt.rand(fake_img.size(0), 1, 1, 1, dtype=fake_img.dtype, device=fake_img.device) + 0.5) + fake_img_mean
+    real_img_mean = real_img.mean(dims=[1, 2, 3], keepdims=True)
+    real_img = (real_img - real_img_mean) * (jt.rand(real_img.size(0), 1, 1, 1) + 0.5) + real_img_mean
+    fake_img_mean = fake_img.mean(dims=[1, 2, 3], keepdims=True)
+    fake_img = (fake_img - fake_img_mean) * (jt.rand(fake_img.size(0), 1, 1, 1) + 0.5) + fake_img_mean
     
     return real_img, fake_img, label
 
@@ -58,36 +58,36 @@ def rand_crop(img, fake,label, strength=0.5):
     label_large = nn.interpolate(label, size=size, mode='nearest')
     _, _, h_large, w_large = img_large.size()
     h_start, w_start = random.randint(0, (h_large - h)), random.randint(0, (w_large - w))
-    print(h_start, w_start)
+    # print(h_start, w_start)
     img_crop = img_large[:, :, h_start:h_start+h, w_start:w_start+w]
     fake_crop = fake_large[:, :, h_start:h_start+h, w_start:w_start+w]
     label_crop = label_large[:, :, h_start:h_start+h, w_start:w_start+w]
     assert img_crop.size() == img.size()
-    is_crop = jt.rand([b, 1, 1, 1], device=img.device) < strength
-    img = jt.where(is_crop, img_crop, img)
-    fake = jt.where(is_crop, fake_crop, fake)
-    label = jt.where(is_crop, label_crop, label)
+    is_crop = jt.rand([b, 1, 1, 1]) < strength
+    img = is_crop*img_crop+is_crop.logical_not()*img
+    fake = is_crop*fake_crop+is_crop.logical_not()*fake
+    label = is_crop*label_crop+is_crop.logical_not()*label
     return img, fake,label
 
 
 def rand_translation(real_img, fake_img, label, ratio=0.125):
     x = real_img
     shift_x, shift_y = int(x.size(2) * ratio + 0.5), int(x.size(3) * ratio + 0.5)
-    translation_x = jt.randint(-shift_x, shift_x + 1, size=[x.size(0), 1, 1], device=x.device)
-    translation_y = jt.randint(-shift_y, shift_y + 1, size=[x.size(0), 1, 1], device=x.device)
+    translation_x = jt.randint(-shift_x, shift_x + 1, shape=[x.size(0), 1, 1])
+    translation_y = jt.randint(-shift_y, shift_y + 1, shape=[x.size(0), 1, 1])
     grid_batch, grid_x, grid_y = jt.meshgrid(
-        jt.arange(x.size(0), dtype=jt.long, device=x.device),
-        jt.arange(x.size(2), dtype=jt.long, device=x.device),
-        jt.arange(x.size(3), dtype=jt.long, device=x.device),
+        jt.arange(x.size(0)).float_auto(),
+        jt.arange(x.size(2)).float_auto(),
+        jt.arange(x.size(3)).float_auto(),
     )
     grid_x = jt.clamp(grid_x + translation_x + 1, 0, x.size(2) + 1)
     grid_y = jt.clamp(grid_y + translation_y + 1, 0, x.size(3) + 1)
     real_img_pad = nn.pad(real_img, [1, 1, 1, 1, 0, 0, 0, 0])
-    real_img = real_img_pad.permute(0, 2, 3, 1).contiguous()[grid_batch, grid_x, grid_y].permute(0, 3, 1, 2).contiguous()
+    real_img = real_img_pad.permute(0, 2, 3, 1)[grid_batch, grid_x, grid_y].permute(0, 3, 1, 2)
     fake_img_pad = nn.pad(fake_img, [1, 1, 1, 1, 0, 0, 0, 0])
-    fake_img = fake_img_pad.permute(0, 2, 3, 1).contiguous()[grid_batch, grid_x, grid_y].permute(0, 3, 1, 2).contiguous()
+    fake_img = fake_img_pad.permute(0, 2, 3, 1)[grid_batch, grid_x, grid_y].permute(0, 3, 1, 2)
     label_pad = nn.pad(label, [1, 1, 1, 1, 0, 0, 0, 0])
-    label = label_pad.permute(0, 2, 3, 1).contiguous()[grid_batch, grid_x, grid_y].permute(0, 3, 1, 2).contiguous()
+    label = label_pad.permute(0, 2, 3, 1)[grid_batch, grid_x, grid_y].permute(0, 3, 1, 2)
     return real_img, fake_img, label
 
 
